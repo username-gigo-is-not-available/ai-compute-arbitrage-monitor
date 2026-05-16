b170b07f20b5abb5907555b7ef3f61a73603f479
grigo <grigorij.josifovski@gmail.com>
2026-05-16 16:14:15 +0200

    Rename dag.py

--- infra/terraform/envs/gcp/composer.tf
+++ infra/terraform/envs/gcp/composer.tf
@@ -26,7 +26,7 @@ resource "google_composer_environment" "composer" {
         DBT_PROJECT_DIR  = "/home/airflow/gcs/dags/transform"
         DBT_PROFILES_DIR = "/home/airflow/gcs/dags/transform"
         DBT_TARGET_DIR   = "/tmp/dbt_target"
-        PYTHONPATH       = "/home/airflow/gcs/dags"
+        PYTHONPATH       = "/home/airflow/gcs/dags:/home/airflow/gcs/dags/src"
       }
     }
 
