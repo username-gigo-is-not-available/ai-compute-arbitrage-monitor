8c461af50cf21952df14b63e0c64acff6f726778
grigo <grigorij.josifovski@gmail.com>
2026-06-28 15:54:45 +0200

    Configure new EVN datasets

--- src/common/enums.py
+++ src/common/enums.py
@@ -21,7 +21,9 @@ class DatasetName(StrEnum):
     COMPUTE_OFFERS = auto()
     EXCHANGE_RATES = auto()
     ELECTRICITY_TARIFF_TIERS = auto()
-    ELECTRICITY_TARIFF_SCHEDULE = auto()
+    ELECTRICITY_TARIFF_FEES = auto()
+    ELECTRICITY_TARIFF_BLOCKS = auto()
+    ELECTRICITY_TARIFF_WINDOW_SCHEDULE = auto()
 
 
 class VerificationStatusType(StrEnum):
@@ -30,7 +32,36 @@ class VerificationStatusType(StrEnum):
     DEVERIFIED = "deverified"
 
 
-class TariffType(StrEnum):
+class ConsumerCategoryType(StrEnum):
+    HOUSEHOLD = auto()
+    BUSINESS = auto()
+
+class TariffBlockType(StrEnum):
+    HT = auto()
+    HT1 = auto()
+    HT2 = auto()
+    HT3 = auto()
+    HT4 = auto()
+    LT = auto()
+
+    @classmethod
+    def from_str(cls, value: str):
+        if value == 'ВТ':
+            return TariffBlockType.HT
+        elif value == 'НТ':
+            return TariffBlockType.LT
+        elif value == 'ВТ 1':
+            return TariffBlockType.HT1
+        elif value == 'ВТ 2':
+            return TariffBlockType.HT2
+        elif value == 'ВТ 3':
+            return TariffBlockType.HT3
+        elif value == 'ВТ 4':
+            return TariffBlockType.HT4
+        else:
+            raise ValueError
+
+class TariffWindowType(StrEnum):
     HIGH = auto()
     LOW = auto()
 
--- src/common/types.py
+++ src/common/types.py
@@ -1,6 +1,5 @@
-from config.apis.erc import ERCConfig
 from config.apis.evn import EVNConfig
 from config.apis.exchange_rate import ExchangeRateConfig
 from config.apis.vast_ai import VastAIConfig
 
-DatasetConfig = VastAIConfig | ExchangeRateConfig | ERCConfig | EVNConfig
+DatasetConfig = VastAIConfig | ExchangeRateConfig | EVNConfig
