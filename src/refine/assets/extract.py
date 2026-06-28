9602e9c3fe3d065e63b992a98051d40ee0f07a14
grigo <grigorij.josifovski@gmail.com>
2026-06-28 16:11:32 +0200

    Add method for pattern extraction

--- src/refine/assets/extract.py
+++
@@ -1,5 +0,0 @@
-from pyspark.sql import functions as F, DataFrame
-
-
-def add_processed_at_column(df: DataFrame) -> DataFrame:
-    return df.withColumn("processed_at", F.to_utc_timestamp(F.current_timestamp(), 'UTC'))
