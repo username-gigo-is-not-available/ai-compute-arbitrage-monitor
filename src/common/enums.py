eb6f029e302a276dd04f72781584b23179843ca7
grigo <grigorij.josifovski@gmail.com>
2026-08-01 17:16:07 +0200

    Remove TariffBlockType

--- src/common/enums.py
+++ src/common/enums.py
@@ -36,30 +36,6 @@ class ConsumerCategoryType(StrEnum):
     HOUSEHOLD = auto()
     BUSINESS = auto()
 
-class TariffBlockType(StrEnum):
-    HT = auto()
-    HT1 = auto()
-    HT2 = auto()
-    HT3 = auto()
-    HT4 = auto()
-    LT = auto()
-
-    @classmethod
-    def from_str(cls, value: str):
-        if value == 'ВТ':
-            return TariffBlockType.HT
-        elif value == 'НТ':
-            return TariffBlockType.LT
-        elif value == 'ВТ 1':
-            return TariffBlockType.HT1
-        elif value == 'ВТ 2':
-            return TariffBlockType.HT2
-        elif value == 'ВТ 3':
-            return TariffBlockType.HT3
-        elif value == 'ВТ 4':
-            return TariffBlockType.HT4
-        else:
-            raise ValueError
 
 class TariffWindowType(StrEnum):
     HIGH = auto()
